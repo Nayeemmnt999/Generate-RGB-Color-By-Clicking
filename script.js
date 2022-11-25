09dae38a3851e7c26a4e6f618146b786462ceb01
Nayeemmnt999 <nayeem.mnt999@gmail.com>
2022-11-25 07:08:25 +0600

    add-function

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-document.getElementById('btn').addEventListener('onclick', function(){
+document.getElementById('btn').addEventListener('click', function(){
     const body = document.getElementById('body');
     const bgColor = randomRGBcolor();
-    body.style.backgroundColor = bgColor;   
+    body.style.backgroundColor = bgColor; 
+    
+    const text = document.getElementById('heading-text')
+    const bgColorText = randomRGBcolorText();
+    text.style.color = bgColorText;
 })
 const randomRGBcolor =()=> {
+    const red=  Math.round(Math.random()*255);
+    const green=  Math.round(Math.random()*255);
+    const blue=  Math.round(Math.random()*255);
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+const randomRGBcolorText =()=> {
     const red=  Math.round(Math.random()*255);
     const green=  Math.round(Math.random()*255);
     const blue=  Math.round(Math.random()*255);
