122cc208c1a9665416af94fa69d3e20e3246043b
Nayeemmnt999 <nayeem.mnt999@gmail.com>
2022-11-25 06:56:24 +0600

    first-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('btn').addEventListener('mouseover', function(){
+    const body = document.getElementById('body');
+    const bgColor = randomRGBcolor();
+    body.style.backgroundColor = bgColor;   
+})
+const randomRGBcolor =()=> {
+    const red=  Math.round(Math.random()*255);
+    const green=  Math.round(Math.random()*255);
+    const blue=  Math.round(Math.random()*255);
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
